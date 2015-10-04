247422f02fb95d58f5930e758018a4eb1caec3fe
bovetliu <bowei_liu@ymail.com>
2015-10-04 16:09:28 -0500

    added login

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+function processReqUser ( req_user){  
+  if (req_user) var temp_user = req_user.toObject();
+  else return null;
+  delete temp_user.password_hash; 
+  return temp_user;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'FASIDS',
-    activePage:'Home'
+    activePage:'Home',
+    isAuthenticated: req.isAuthenticated(),
+    user: processReqUser(req.user)
   });
 });
 
 router.get('/questions', function (req, res, next){
   res.render('qa', {title:'Question and Answers | FASIDS',
-    activePage:'Questions'
+    activePage:'Questions',
+    isAuthenticated: req.isAuthenticated(),
+    user: processReqUser(req.user)
   });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* all the routes under "/users"*/
+/* */
+var passport = null;
+router.all('*', function(req, res, next) {
+  if (passport === null){
+    passport = req.PASSPORT;
+  }
+  return next(); // following routes conninue handling requests
 });
 
 router.get('/signup', function (req, res, next){
-  res.render("signup",null);
+  res.render("signup",{
+    breadcrumTitle:"sign up",
+    pathToHere:"users / signup"
+  });
 });
+
+router.post('/signup', function (req, res, next){
+  /*db operation goes here*/
+  var new_user = {};
+  new_user.email = req.body.email;
+  new_user.password_hash = req.body.password; 
+  new_user.first_name = req.body.firstname;
+  new_user.last_name = req.body.lastname;
+  // new_user.receive_updates = req.body.receive_updates|| false;
+  new_user.usercat = req.body.usercat;     // 1 is common user
+  
+  new_user = new req.DB_USER(new_user);
+  new_user.save(function (error){
+    if (error) return next(error);
+    console.log("[user.js:34 ]");
+    // res.json(new_user.email);   // TODO send email here
+  });
+  res.render("signup",{
+    breadcrumTitle:"sign up",
+    pathToHere:"users / signup",
+    msg_type:"success",
+    msg_content:"Hello, "+new_user.displayName()+", you've signed up successfully. Click \"SIGN IN\" at left upper corner to sign in"
+  });
+});
+
+router.get('/logout', function (req, res, next){
+  req.logout();
+  res.redirect("back");
+})
+
+// for  /users/signin
+// router.post('/signin', passport.authenticate('local'),function (req, res, next){
+//   console.log("[user.js 47]");
+//   res.redirect('back'); 
+// });
 
 module.exports = router;
