9aa5142c3e5c549ea92fd08467ac28580a653840
bovetliu <bowei_liu@ymail.com>
2015-10-05 15:04:51 -0500

    implementing qa

---
+++ db_models/post.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+var _ = require('underscore');
+var post_schema = new mongoose.Schema({
+  "role":Number, // 1 means main_post, 2 means reply
+  "post_id":String,
+  "poster_id":mongoose.Schema.ObjectId,
+  "post_cat":{type:Number, min:0, max:4},   // used for the icons
+  "post_title":String,
+  "post_time": Date,
+  "reply_to_post":String,// post created to reply specific post
+  "reply_to_mainpost":String,
+  "content":String
+},{ collection:'post'});
+
+// define instance methods
+post_schema.method({
+
+});
+
+
+post_schema.static({
+  getAllFollowUps: function( main_post_id, callback){
+    var Model = this;
+    Model.findOne({"post_id": main_post_id}, null,{},function (err, instance){
+      if (err) throw (err);
+      if (instance.role !== 1){
+        throw ("only main post can have follow ups");
+      }
+      Model.find({"reply_to_mainpost": main_post_id}).sort({"post_time":-1}).exec(callback);
+    });
+  },
+  getAllMainPost: function ( callback) {   // I can put sort parameter here
+    var Model = this;
+    Model.find({"post_cat":1}).sort({"post_time":-1}).exec(callback);
+  }
+});
+
+// helper function of schema
+var saveCB = function( err, instance){
+  if (err) console.log("[! ERROR !]  NOT successfully saved");
+  else console.log("[INFO] "+ instance._id +" saved. ");
+};
+
+module.exports = mongoose.model('Post',post_schema);
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ function ensureAuthenticated(req, res, next) {
     // var my_error = new Error("Unauthorized behaviro");
     // my_error.status = 401;
     // next(my_error);
-    res.status(401).send("Unauthorized action, loggin required");
+    res.status(401).send("Unauthorized action, login required");
   }
 }
 
@@ -43,13 +43,46 @@ router.get('/qa', function (req, res, next){
 /* /qa/question?qid=123  */
 router.get('/qa/question',function (req, res, next){
   console.log(req.query.qid);
-  res.render('question',{
+  res.render('question.jade',{
     breadcrumTitle:"Question title(TODO: retrieve from DB)",
     pathToHere:"qa / question?id=12345678",
     title: 'QA POSTING | FASIDS',
     activePage:'Questions',
     isAuthenticated: req.isAuthenticated(),
     user: processReqUser(req.user)
+  });
+});
+
+/*
+  "role":Number, // 1 means main_post, 2 means reply
+  "post_id":String,
+  "poster_id":mongoose.Schema.ObjectId,
+  "post_cat":{type:Number, min:0, max:4},   // used for the icons
+  "post_title":String,
+  "post_time": Date,
+  "reply_to_post":String,// post created to reply specific post
+  "reply_to_mainpost":String,
+  "content":String*/
+router.post('/qa/posting', ensureAuthenticated, function (req, res, next){
+  console.log("$$$$$$posting:$$$");
+  console.log(req.user._id);
+  var currentDate = new Date();
+  var post_id = (currentDate.getFullYear()%2000).toString() + (currentDate.getMonth()+1).toString() + currentDate.getDay().toString() + currentDate.getHours().toString();
+  post_id += currentDate.getMinutes().toString() + currentDate.getSeconds().toString() + currentDate.getMilliseconds().toString();
+
+  var newPost = {
+    role:1,
+    post_id:post_id,
+    poster_id: req.user._id,
+    post_cat:(req.body.post_cat)?parseInt(req.body.post_cat):1,
+    post_title:req.body.title,
+    post_time:currentDate,
+    content: req.body.content
+  };
+  newPost = new req.DB_POST(newPost);
+  newPost.save( function (error){
+    if (error) return next(error);
+    return res.redirect('/qa');
   });
 });
 
