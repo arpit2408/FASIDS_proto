fb23a97c30c03aef35da1bd24002969b11cae646
bovetliu <bowei_liu@ymail.com>
2015-10-14 13:40:34 -0500

    can now input address to see ant forecast now

--- public/js/antactivity.js
+++ public/js/antactivity.js
@@ -13,6 +13,21 @@ $(document).ready(function(){
   var current = {};
   
   var helper_util = {
+    url:function(mode){
+      if (mode === "weather" || mode === "forecast"){
+        return "http://api.openweathermap.org/data/2.5/" + mode;      
+      }
+      else throw "mode parameter error";
+    },
+    get_query:{
+      q:"college station,tx,us",
+      mode:"json",
+      appid:"64bb5bb6fbdeb48105b321b7c6ddae37"
+    },
+    makeQuery:function( mode){
+      var scope = this;
+      return scope.url(mode)+"?"+$.param(scope.get_query);
+    },
     avg:function (one_list){
       var i = 0;
       var sum = 0;
@@ -24,7 +39,7 @@ $(document).ready(function(){
     },
     // I should have one function  function (TEMPERATURE, HUMIDITY), which return ANT_TIVITY 
     tell_activity: function(date_summary){
-      if (date_summary.temp_avg >= 280 && date_summary.humidity_avg > 60){
+      if (date_summary.temp_avg >= 290 && date_summary.humidity_avg > 30){
         return "high";
       } else if (date_summary.temp_avg >= 270 && date_summary.temp_avg < 280 ) {
         return "middle";
@@ -36,81 +51,100 @@ $(document).ready(function(){
       return _.map(date_list, function(period_data){
         return period_data.main[attr];
       });
+    },
+    refresh:function(){
+
+
+      var scope = this;
+      $.getJSON(scope.makeQuery("forecast"), function (data){
+        $('#calendar').fullCalendar('removeEvents');
+        forecast = data;
+        // get future 4 days
+        var i =0 ;
+        var forecast4 = [[],[],[],[],[],[]];
+        data.list.forEach(function(element, index, ar){
+          var tempDate = new Date(element.dt * 1000);
+          forecast4[tempDate.getDate() - crtDate.getDate() ].push(element);
+        });
+
+        forecast4 = forecast4.slice(1,5);
+        
+        //prepare forecast4_daysummary
+        var forecast4_daysummary = [];
+        for (i= 0; i<forecast4.length; i ++){
+          //console.log(forecast4[i], null, "  ");
+          var temp_list = helper_util.map_main(forecast4[i], "temp");
+          var humidity_list = helper_util.map_main(forecast4[i], "humidity");
+          forecast4_daysummary[i] = {
+            date: moment().add(i+1,'days').format('YYYY-MM-DD') ,
+            temp_avg:helper_util.avg(temp_list),
+            temp_max: _.max( temp_list),
+            temp_min: _.min( temp_list),
+            humidity_avg: helper_util.avg(  humidity_list )
+          };
+          console.log( JSON.stringify(forecast4_daysummary[i] ,null,"  "));
+        }
+
+        // adding event to forecast4_summary
+        _.each(forecast4_daysummary, function(element, index, array){
+          // serves as bg color
+          var date_event = {
+            start: moment().add(index+1, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
+            end:moment().add(index+2, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
+            allDay: true,
+            rendering:'background'
+          };
+          var display_text_event = {
+            start: moment().add(index+1, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
+            end: moment().add(index+2, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
+            // color:"#66FF33",
+            allDay:true,
+            title:"Ant Activity:",
+            textColor:"#000000",
+            backgroundColor:"rgba(255, 0, 0, 0.0)",
+            borderColor:"rgba(255,0,0,0.0)",
+            className:"activity-text"
+          }
+          if (helper_util.tell_activity(element) === "high"){
+            date_event.backgroundColor= "#FFB2B2";
+            display_text_event.textColor = "#FF0000";
+            display_text_event.title += " high";
+          }
+          else if (helper_util.tell_activity(element) === "middle" ){
+            date_event.backgroundColor= "#FFD699";
+            display_text_event.title += " middle";
+          }
+          else {
+            date_event.backgroundColor= "#ADEBAD";
+            display_text_event.title += " low";
+          }
+          $('#calendar').fullCalendar("renderEvent", date_event , true);
+
+
+          $('#calendar').fullCalendar("renderEvent", display_text_event , true);
+
+        });
+
+      }); 
+
+      $.getJSON(scope.makeQuery("weather"), function (data){
+        current = data;
+
+      });
     }
 
-  };
-
-  $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=json&appid=64bb5bb6fbdeb48105b321b7c6ddae37", function (data){
-    forecast = data;
-    // get future 4 days
-    var i =0 ;
-    var forecast4 = [[],[],[],[],[],[]];
-    data.list.forEach(function(element, index, ar){
-      var tempDate = new Date(element.dt * 1000);
-      forecast4[tempDate.getDate() - crtDate.getDate() ].push(element);
-    });
-
-    forecast4 = forecast4.slice(1,5);
-    
-    var forecast4_daysummary = [];
-    for (i= 0; i<forecast4.length; i ++){
-      //console.log(forecast4[i], null, "  ");
-      var temp_list = helper_util.map_main(forecast4[i], "temp");
-      var humidity_list = helper_util.map_main(forecast4[i], "humidity");
-      forecast4_daysummary[i] = {
-        date: moment().add(i+1,'days').format('YYYY-MM-DD') ,
-        temp_avg:helper_util.avg(temp_list),
-        temp_max: _.max( temp_list),
-        temp_min: _.min( temp_list),
-        humidity_avg: helper_util.avg(  humidity_list )
-      };
-      console.log(forecast4_daysummary[i]);
+  }; // end of helper_util
+  helper_util.refresh();
+  $("#refresh-btn").click(function refreshBtnClicked(){
+    var $this = $(this);
+    if ($("#query-place-ipt").val() ==""){
+      helper_util.get_query.q = "college station,tx,us";
+    } else{
+      helper_util.get_query.q = $("#query-place-ipt").val();
     }
 
-    _.each(forecast4_daysummary, function(element, index, array){
-      var date_event = {
-        start: moment().add(index+1, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
-        end:moment().add(index+2, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
-        allDay: true,
-        rendering:'background'
-      };
-
-      if (helper_util.tell_activity(element) === "high"){
-
-        date_event.backgroundColor= "#FFB2B2";
-      }
-      if (helper_util.tell_activity(element) === "middle" ){
-
-        date_event.backgroundColor= "#FFD699";
-      }
-      if (helper_util.tell_activity(element) === "low" ){
-
-        date_event.backgroundColor= "#ADEBAD";
-      }
-
-      $('#calendar').fullCalendar("renderEvent", date_event , true);
-    });
-
-  }); 
-  $('#calendar').fullCalendar({
-      // put your options and callbacks here
-      eventClick:function(event, element){
-        console.log(element);
-      }
+    helper_util.refresh();
+    console.log("refresh-btn clicked");
   });
-
-  // var date_event = {
-  //   start: moment().add(1, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
-  //   end:moment().add(2, "days").format("YYYY-MM-DD") + "T00:00:00-05:00",
-  //   allDay: true,
-  //   backgroundColor:"#FFD699",
-  //   rendering:'background'
-  // };
-  // $('#calendar').fullCalendar("renderEvent", date_event );
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London,us&mode=json&appid=64bb5bb6fbdeb48105b321b7c6ddae37", function (data){
-    current = data;
-
-  });
-
-
+  
 });
