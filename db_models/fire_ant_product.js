857841cef71686475cae4f3a241eaf8aae70c2ad
bovetliu <bowei_liu@ymail.com>
2015-10-22 16:46:15 -0500

    starting add product collection

---
+++ db_models/fire_ant_product.js
@@ -0,0 +1,35 @@
+// collection name : fire_ant_product
+// Mongoose Class Name   FireAntProduct
+
+var mongoose = require('mongoose');
+var _ = require('underscore');
+var fire_ant_product_schema = new mongoose.Schema({
+  product_id:Number,           // get from homedeport Internet #
+  product_name: String,
+  manufacturer: String,
+  application_type: String,    //  granular, dust
+  pest_type: String,           //  comma seprated string array
+  con_or_rtu: String,          // 'rtu' means ready to use, 'con' means concentrated, need dilution before using
+  is_outdoor: Boolean,
+  is_safe_editable: Boolean,
+  is_safe_for_pets: Boolean,
+  control_method: String,       // 'bait', 'contact'
+  usage: String,                // 'broadcast', 'mound treatment'
+  coverage: Number,             // if it is broadcast, this one indicates the area in square meter unit, if it is mound treatment, it is how many mounds it can cover 
+  is_water_needed: Boolean,   
+  homedepot_score: Number, 
+  product_overview: String,
+},{ collection:'fire_ant_product'});
+
+// define instance methods
+fire_ant_product_schema.method({
+
+});
+
+// helper function of schema
+var saveCB = function( err, instance){
+  if (err) console.log("[! ERROR !]  NOT successfully saved");
+  else console.log("[INFO] "+ instance._id +" saved. ");
+};
+
+module.exports = mongoose.model('FireAntProduct',fire_ant_product_schema);
--- db_models/index.js
+++ db_models/index.js
@@ -1,2 +1,3 @@
 exports.User = require('./user.js');
 exports.Post = require('./post.js');
+exports.FireAntProduct = require('./fire_ant_product.js')
