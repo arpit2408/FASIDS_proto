4c893b06b1e18cc67884d119933586b2847e5360
bovetliu <bowei_liu@ymail.com>
2016-01-12 10:35:29 -0600

    save work 01122016

--- routes/blog.js
+++ routes/blog.js
@@ -3,11 +3,21 @@ var router = require('express').Router();
 var glblprefix = (process.env.NONEIISNODE) ? "":"/node/fasids";
 var routesHelpers = require('./routesHelpers');
 
+router.get('/', function (req, res, next){
+  res.render('blog/index', {
+    title: 'Index | Blog | FASIDS',
+    activePage:'Blog',
+    breadcrumTitle:"Index of blogs",
+    pathToHere:"blogs",
+    isAuthenticated: req.isAuthenticated(),
+    user: routesHelpers.processReqUser(req.user) 
+  });
+});
 
 router.get('/first-blog', function (req, res, next) {
   res.render('blog/first-blog', {
     title: 'BLOG',
-    activePage:'Reports',
+    activePage:'Blog',
     breadcrumTitle:"How can I tell if I have fire ants?",
     pathToHere:"blogs / first-blog",
     isAuthenticated: req.isAuthenticated(),
